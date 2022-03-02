2825570ab5bfec1fd9d8261739749445f6db47a6
Paulina Ostolska <pa.ostolska@gmail.com>
2022-03-02 18:16:07 +0100

    Initial Commit

---
+++ StosTab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StosTab
+//
+//  Created by Paula Ostolska on 02/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
